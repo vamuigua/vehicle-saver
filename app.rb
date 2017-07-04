e892cf53c1ad88efc2b3b1087dd71e6fd6c3c9cc
Victor Allen <vamuigua@gmail.com>
2017-07-04 12:20:55 +0300

    Added app.rb file to load routes required

---
+++ app.rb
@@ -0,0 +1,31 @@
+require('sinatra')
+  require('sinatra/reloader')
+  also_reload('lib/**/*.rb')
+  require('./lib/vehicle')
+
+  get('/') do
+  erb(:index)
+  end
+
+  get('/vehicles') do
+  @vehicles = Vehicle.all()
+  erb(:vehicles)
+  end
+
+  get('/vehicles/new') do
+  erb(:vehicles_form)
+  end
+
+  post('/vehicles') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  erb(:success)
+  end
+
+  get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch("id"))
+  erb(:vehicle)
+  end
