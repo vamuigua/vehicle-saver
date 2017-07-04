b0a9e617a2d0026260a240d3c646ff8e9e3d5b2a
Victor Allen <vamuigua@gmail.com>
2017-07-04 16:05:50 +0300

    modified the app.rb

--- app.rb
+++ app.rb
@@ -2,31 +2,50 @@ require('sinatra')
   require('sinatra/reloader')
   also_reload('lib/**/*.rb')
   require('./lib/vehicle')
+  require('./lib/dealership')
 
   get('/') do
-  erb(:index)
+    erb(:index)
   end
 
-  get('/vehicles') do
-  @vehicles = Vehicle.all()
-  erb(:vehicles)
+  get('/dealerships/new') do
+     erb(:dealerships_form)
   end
 
-  get('/vehicles/new') do
-  erb(:vehicles_form)
+  get('/dealerships') do
+    @dealerships = Dealership.all()
+    erb(:dealerships)
+  end
+
+  post('/dealerships') do
+    name = params.fetch('name')
+    Dealership.new(name).save()
+    @dealerships = Dealership.all()
+    erb(:success)
+  end
+
+  get('/vehicles/:id') do
+    @vehicle = Vehicle.find(params.fetch('id').to_i())
+    erb(:vehicle)
+  end
+
+  get('/dealerships/:id') do
+    @dealership = Dealership.find(params.fetch('id').to_i())
+    erb(:dealership)
+  end
+
+  get('/dealerships/:id/vehicles/new') do
+      @dealership = Dealership.find(params.fetch('id').to_i())
+      erb(:dealership_vehicles_form)
   end
 
   post('/vehicles') do
-  make = params.fetch("make")
-  model = params.fetch("model")
-  year = params.fetch("year")
-  vehicle = Vehicle.new(make, model, year)
-  vehicle.save()
-  erb(:success)
-  end
-
-#route to specifications of a particlular vehicle
-  get('/vehicles/:id') do
-  @vehicle = Vehicle.find(params.fetch("id"))
-  erb(:vehicle)
+    make = params.fetch('make')
+    model = params.fetch('model')
+    year = params.fetch('year')
+    @vehicle = Vehicle.new(make, model, year)
+    @vehicle.save()
+    @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+    @dealership.add_vehicle(@vehicle)
+    erb(:success)
   end
