94f748d9e6d3561a7f4b1e82c01c413900cc3fb6
Victor Allen <vamuigua@gmail.com>
2017-07-04 11:44:17 +0300

    Added spec to check the id of a particular vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -5,7 +5,8 @@ require('rspec')
     before() do
       Vehicle.clear()
     end
-
+      
+    #spec to make a new vehicle instance
     describe("#make") do
       it("returns the make of the vehicle") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
@@ -13,6 +14,7 @@ require('rspec')
       end
     end
 
+    #spec check model of vehicle
     describe("#model") do
       it("returns the model of the vehicle") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
@@ -20,6 +22,7 @@ require('rspec')
       end
     end
 
+    #spec to check the year the vehicle was made
     describe("#year") do
       it("returns the year of the vehicle") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
@@ -27,12 +30,14 @@ require('rspec')
       end
     end
 
-  describe(".all") do
+    #spec to check vehicles in the array list  
+    describe(".all") do
       it("is empty at first") do
         expect(Vehicle.all()).to(eq([]))
       end
     end
 
+    #spec to save new vehicle instances
    describe("#save") do
       it("adds a vehicle to the array of saved vehicles") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
@@ -41,6 +46,7 @@ require('rspec')
       end
     end
 
+    #spec to clear the vehicles array list
    describe(".clear") do
       it("empties out all of the saved vehicles") do
         Vehicle.new("Toyota", "Prius", 2000).save()
@@ -48,14 +54,15 @@ require('rspec')
         expect(Vehicle.all()).to(eq([]))
       end
     end
-
+    
+    #spec to check the age of the vehicle
     describe("#age") do
       it("returns the vehicles age") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
         expect(test_vehicle.age()).to(eq(17))
       end
     end
-
+    #spec to check if vehicle is worth-buying
     describe("#worth_buying?") do
       it("returns false if the car is not American made and less than 15 years old") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
@@ -63,10 +70,22 @@ require('rspec')
       end
     end
     
+    #spec to give the id of the vehicle
     describe("#id") do
       it("returns the id of the vehicle") do
         test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
         test_vehicle.save()
         expect(test_vehicle.id()).to(eq(1))
       end
+    
+    #spec to find each vehicle by id
+    describe(".find") do
+    it("returns a vehicle by its id number") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      test_vehicle2 = Vehicle.new("Toyota", "Corolla", 1999)
+      test_vehicle2.save()
+      expect(Vehicle.find(test_vehicle.id())).to(eq(test_vehicle))
     end
+  end
+  
