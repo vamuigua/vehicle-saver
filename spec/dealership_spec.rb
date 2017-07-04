65209fbcde8555637635039e0acafba0e90e53d5
Victor Allen <vamuigua@gmail.com>
2017-07-04 15:36:26 +0300

    Added method to spec to add vehicles to our array

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -1,5 +1,6 @@
 require('rspec')
   require('dealership')
+  require('vehicle')
 
   describe(Dealership) do
     before() do
@@ -58,4 +59,12 @@ require('rspec')
         expect(Dealership.find(test_dealership.id())).to(eq(test_dealership))
       end
     end
-  end
+    
+    describe('#add_vehicle') do
+      it("adds a new vehicle to a dealership") do
+        test_dealership = Dealership.new("Bob's Used Cars")
+        test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+        test_dealership.add_vehicle(test_vehicle)
+        expect(test_dealership.cars()).to(eq([test_vehicle]))
+      end
+    end
